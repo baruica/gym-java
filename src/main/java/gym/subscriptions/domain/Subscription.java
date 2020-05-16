ffebfb60249461673d066d4771279ca69372b371
Nelson <baruica@gmail.com>
2020-05-16 11:49:18 +0200

    Discount and Price become inner classes of Subscription

--- src/main/java/gym/subscriptions/domain/Subscription.java
+++ src/main/java/gym/subscriptions/domain/Subscription.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class Subscription {
 
@@ -61,5 +62,54 @@ public final class Subscription {
 
     public Double monthlyTurnover() {
         return (double) (price / planDurationInMonths);
+    }
+
+    private static final class Price {
+
+        private final Integer amount;
+
+        Price(Integer amount) {
+            this.amount = amount;
+        }
+
+        Integer afterDiscount(Integer durationInMonths, Boolean isStudent) {
+            return (int) (amount * (1 - new Discount(durationInMonths, isStudent).rate()));
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(amount.intValue());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Price price = (Price) o;
+            return Objects.equals(amount, price.amount);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(amount);
+        }
+    }
+
+    private static final class Discount {
+
+        private Double rate = 0.0;
+
+        Discount(final Integer durationInMonths, final Boolean isStudent) {
+            if (durationInMonths == 12) {
+                rate += 0.3;
+            }
+            if (isStudent) {
+                rate += 0.2;
+            }
+        }
+
+        Double rate() {
+            return rate;
+        }
     }
 }
