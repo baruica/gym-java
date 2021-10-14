11d483529913b3a58338d2380d728e622127c8d5
Nelson da Costa <baruica@gmail.com>
2021-10-14 17:12:29 +0200

    switch from maven to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+}
+
+group = "me.baruica"
+version = "1.0-SNAPSHOT"
+description = "The gym"
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
