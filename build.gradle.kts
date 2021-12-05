a6f78eb474140ec0dd629caa744c8e7e6aad2b55
Nelson <baruica@gmail.com>
2021-12-05 10:04:14 +0100

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.withType<Test> {
