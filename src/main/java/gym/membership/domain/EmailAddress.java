45443d74f2924c40a0cb73eb0b9fdcf4ca7595a5
Nelson <baruica@gmail.com>
2022-04-18 23:20:04 +0200

    refactor: simplify email address regex

--- src/main/java/gym/membership/domain/EmailAddress.java
+++ src/main/java/gym/membership/domain/EmailAddress.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public record EmailAddress(String value) {
 
     public EmailAddress {
-        if (!Pattern.matches("^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$", value)) {
+        if (!Pattern.matches("^[\\w-_.+]*[\\w-_.]@(\\w+\\.)+\\w+\\w$", value)) {
             throw new IllegalArgumentException("EmailAddress must be a valid email address, was [" + value + "]");
         }
     }
