8f8862aa2cdaa58dce625724a7d7fde27b72cd86
Nelson <baruica@gmail.com>
2023-04-16 17:28:10 +0200

    chore: update to java 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
 }
 
 tasks.withType<Test> {
