d71529298112023fb0f6f1fee5d3577b395567a5
Nelson <baruica@gmail.com>
2023-04-16 18:11:13 +0200

    chore: don't use java toolchain to set java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,8 @@ dependencies {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
-    }
+    sourceCompatibility = JavaVersion.VERSION_20
+    targetCompatibility = JavaVersion.VERSION_20
 }
 
 tasks.withType<Test> {
